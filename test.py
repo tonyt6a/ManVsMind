85e3f7148cddfda27a984f9b4b9f0b512ac6968b
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2024-11-02 18:04:33 -0500

    Started game dev

--- test.py
+++ test.py
@@ -1,30 +1,36 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_TITLE = "Man Vs. Mind"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
 
 
 class MyGame(arcade.Window):
     """
     Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
     """
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color(arcade.color.AMAZON)
+        self.wall_list = None
+        self.player_list = None
 
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+        self.player_sprite = None
+
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
-        # Create your sprites and sprite lists here
+        #create the sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        #set up the player
+        image_source = 
         pass
 
     def on_draw(self):
@@ -38,46 +44,46 @@ class MyGame(arcade.Window):
 
         # Call draw() on all your sprite lists below
 
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
+    # def on_update(self, delta_time):
+    #     """
+    #     All the logic to move, and the game logic goes here.
+    #     Normally, you'll call update() on the sprite lists that
+    #     need it.
+    #     """
+    #     pass
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+    # def on_key_press(self, key, key_modifiers):
+    #     """
+    #     Called whenever a key on the keyboard is pressed.
 
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
+    #     For a full list of keys, see:
+    #     https://api.arcade.academy/en/latest/arcade.key.html
+    #     """
+    #     pass
 
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
+    # def on_key_release(self, key, key_modifiers):
+    #     """
+    #     Called whenever the user lets off a previously pressed key.
+    #     """
+    #     pass
 
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
+    # def on_mouse_motion(self, x, y, delta_x, delta_y):
+    #     """
+    #     Called whenever the mouse moves.
+    #     """
+    #     pass
 
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
+    # def on_mouse_press(self, x, y, button, key_modifiers):
+    #     """
+    #     Called when the user presses a mouse button.
+    #     """
+    #     pass
 
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
+    # def on_mouse_release(self, x, y, button, key_modifiers):
+    #     """
+    #     Called when a user releases a mouse button.
+    #     """
+    #     pass
 
 
 def main():
